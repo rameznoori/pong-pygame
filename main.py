8327d7bb58a891eaf9f37f0bf733a6912ea48b7a
rameznoori <199920071+rameznoori@users.noreply.github.com>
2025-09-13 15:06:50 +0530

    added variable for game paddles, the ball, and speed of the ball

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ def main():
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Pong")
 
+    paddle_1_rect = pygame.Rect(30,0,7,100)
+    paddle_2_rect = pygame.Rect(screen_width - 50,0,7,100)
+    paddle_1_move = 0
+    paddle_2_move = 0
+    ball_rect = pygame.Rect(screen_width/2, screen_height/2, 25,25)
+    ball_accel_x = random.randint(2,4)*0.1
+    ball_accel_y = random.randint(2,4)*0.1
+
     while True:
         screen.fill(color_black)
         for event in pygame.event.get():
